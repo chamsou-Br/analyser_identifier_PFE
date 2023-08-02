38ec9e268fc9754371d895c58619f1e48fb4875e
Abdelouadoud8 <a.mahdaoui@esi-sba.dz>
2023-08-02 13:05:14 +0200

    ♻️ Refactoring of controllers

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   class ApiController < ActionController::Base
-    def set_connexion(session_id, endpoint)
+    def get_current_customer(session_id, endpoint)
       api_url = "http://localhost:3000/#{endpoint}"
 
       headers = {
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,89 +3,74 @@ class ApplicationController < ActionController::Base
 
   skip_before_action :verify_authenticity_token
 
-  helper_method :set_connexion
+  helper_method :get_current_customer
   helper_method :get_token
 
-
+  # Initiation of class variables
   @@session_token = nil
   @@memorized_session_data_length = 0
-
+  MSQUALIPSO_BASE_URL = "http://localhost:3000".freeze
 
   # Returns the current session token
   def get_token
     # Checks if we don't have other new tokens, we return directly same token
-    if (@@session_token && redis_session_data_length == @@memorized_session_data_length)
-      puts "No other token is added"
-      return @@session_token
-    end
+    @@session_token &&= @@session_token if redis_session_data_length == @@memorized_session_data_length
 
-    puts "New token available we are searching it"
-    redis_config = {
-      url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'),
-      namespace: "pyx4_#{Rails.env}"
-    }.freeze
-    redis = Redis.new(redis_config) # Establish the connection to Redis
-    session_data = redis.keys("qualipso_#{Rails.env}:session:*") # Retrieve the key
-    puts "retrieved all keys"
+    session_data = redis_connection.keys("qualipso_#{Rails.env}:session:*") # Retrieve the key
     @@memorized_session_data_length = session_data.length # Memorize the session_data length
 
     # Test all tokens, retrieve the right token
     if (session_data.length == 1)
       @@session_token = session_data[0].match(/:session:(\w+)/)[1]
-      return @@session_token
     else
-      for n in 0..session_data.length-1 do
-        current_token = session_data[n].match(/:session:(\w+)/)[1]
-        puts "testing: #{current_token}"
-        if (test_token(current_token))
+      session_data.each do |key|
+        current_token = key.match(/:session:(\w+)/)[1]
+        if test_token(current_token)
           @@session_token = current_token
-          puts "token: #{@@session_token} is working"
-          return @@session_token
+          break
         end
       end
     end
+
+    return @@session_token
   end
 
   # Tests if the token works
   def test_token(token)
-    puts "Testing"
-    api_url = "http://localhost:3000/api/test_token"
+    api_url = "#{MSQUALIPSO_BASE_URL}/api/test_token"
     headers = {'Cookie' => "_qualipso_session=#{token}"}
     customer = HTTParty.get(api_url, headers: headers).to_s
-    puts "Tested"
 
-    if (customer[0] == "w" && customer[1] == "o")
-      is_good = true
-    else
-      is_good = false
-    end
-    return is_good
+    customer[0] == "w" && customer[1] == "o"
   end
 
-
-  def set_connexion
-
+  # Retrieves the current_customer
+  def get_current_customer
     session_id = get_token
     headers = {'Cookie' => "_qualipso_session=#{session_id}"}
-    current_customer_endpoint = 'get_current_customer'
-    api_url = "http://localhost:3000/#{current_customer_endpoint}"
+    api_url = "#{MSQUALIPSO_BASE_URL}/get_current_customer"
 
     customer = HTTParty.get(api_url, headers: headers, timeout: 20)
     user = Customer.find(customer['id'])
-    return user
+    user
   end
 
   private
 
+  # returns the number of session keys available
   def redis_session_data_length
-    redis_config = {
-      url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'),
-      namespace: "pyx4_#{Rails.env}"
-    }.freeze
-    redis = Redis.new(redis_config) # Establish the connection to Redis
-    session_data = redis.keys('qualipso_development:session:*') # Retrieve the key
-    puts session_data.length
+    session_data = redis_connection.keys('qualipso_development:session:*') # Retrieve the key
     session_data.length
   end
 
+  # Connection to redis
+  def redis_connection
+    @redis_connection ||= begin
+      redis_config = {
+        url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'),
+        namespace: "pyx4_#{Rails.env}"
+      }.freeze
+      Redis.new(redis_config)
+    end
+  end
 end
