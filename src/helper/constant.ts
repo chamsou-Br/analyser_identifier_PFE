f69584e8cb12a92b39d4af299d0ec33e93519273
chamsou-Br <jc_berkane@esi.dz>
2024-01-31 15:36:55 +0100

    modify server API

--- src/helper/constant.ts
+++ src/helper/constant.ts
@@ -1,6 +1,6 @@
 import { DeliveryType } from "./types";
 
-export const BASE_URL = "http://localhost:7550/api" 
+export const BASE_URL = "https://api.paypart.dz/api" // "http://localhost:7550/api" 
 
 export const tokenName = "pepper_token";
 
